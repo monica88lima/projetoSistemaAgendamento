a838dbb8eae0e9c78c931ff8c986b3e8defe7c6e
monica88lima <rishidovalle@gmail.com>
2023-01-16 11:57:41 -0300

    commit inicial

---
+++ SistemaAgendamentoIndividual/SistemaAgendamentoIndividual/Entidades/Base.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAgendamentoIndividual.Entidades
+{
+    public abstract class Base
+    {
+        public int Id { get; set; }
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+        public bool Status { get; set; }
+    }
+}
